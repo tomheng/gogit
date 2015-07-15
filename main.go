1274704b7e4d4182cd9fda74af126f964852736e
tomheng <zhm20070928@gmail.com>
2015-07-15 18:01:22 +0800

    init

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+	/*conn, err := net.Dial("tcp", "127.0.0.1:9418")
+	if err != nil {
+		fmt.Println("one")
+		panic(err)
+	}
+	_, err = conn.Write([]byte("0032git-upload-pack /git-bottom-up\000host=localhost\000"))
+	if err != nil {
+		fmt.Println("2")
+		panic(err)
+	}
+	resp := make([]byte, 1024)
+	for{
+		_, err = conn.Read(resp)
+		if err != nil {
+			fmt.Println("3")
+			panic(err)
+		}
+		fmt.Println(string(resp))
+	}*/
+	remote := newRemote("git://127.0.0.1/flybird")
+	remote.LsRemote()
+}
---
+++ remote.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	
+	"github.com/bargez/pktline"
+)
+
+const (
+	FLUSH_PACKET = "0000"
+)
+
+type Remote struct {
+	Host string
+	Port string
+	Repo string
+}
+
+func (r *Remote) LsRemote() {
+	conn := r.connect()
+	defer conn.Close()
+	pktLineEder := pktline.NewEncoderDecoder(conn)
+	cmd := r.getCmd("upload-pack")
+	err := pktLineEder.Encode(cmd)
+	if err != nil {
+		panic(err)
+	}
+	pktLines := make([][]byte, 0)
+	err = pktLineEder.DecodeUntilFlush(&pktLines)
+	pktLineEder.Encode(nil)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range pktLines{
+		fmt.Println(string(v))
+	}
+}
+
+func (r *Remote ) getCmd(git_cmd string) []byte {
+	//0032git-upload-pack /git-bottom-up\0Host=localHost\0
+	msg := fmt.Sprintf("git-%s %s\000Host=%s\000", git_cmd, r.Repo, r.Host)
+	return []byte(msg)
+}
+
+func (r *Remote) connect() (conn net.Conn) {
+	conn, err := net.Dial("tcp", net.JoinHostPort(r.Host, r.Port))
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func newRemote(git_url string) *Remote {
+	gurl, err := url.Parse(git_url)
+	if err != nil {
+		panic(err)
+	}
+	host, port, err := net.SplitHostPort(gurl.Host)
+	if err != nil {
+		port = "9418"
+		host = gurl.Host
+	}
+	return &Remote{
+		host,
+		port,
+		gurl.Path,
+	}
+}
